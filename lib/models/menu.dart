ca519ed6db841dd12d2c6e8efc901b56be59f5a6
cr0manty <aappland@gmail.com>
2020-07-26 23:07:47 +0300

    add menu

--- lib/models/menu.dart
+++
@@ -1,8 +0,0 @@
-class News {
-  String title;
-  String description;
-  double price;
-  List<String> images;
-
-  News({this.price, this.description, this.images, this.title});
-}
---
+++ lib/models/menu_item.dart
@@ -0,0 +1,8 @@
+class MenuItem {
+  String title;
+  String description;
+  double price;
+  List<String> images;
+
+  MenuItem({this.price, this.description, this.images, this.title});
+}
